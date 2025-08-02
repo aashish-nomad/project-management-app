79b7d8e62650c0f635b23418eece0a7d6a5a9b66
aashish-nomad <aashish.nomad@gmail.com>
2025-08-02 21:13:10 +0545

    Adds project state and logic to toggle between Project form component and NoProject selected component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,35 @@
+import { useState } from "react";
 import NewProject from "./components/NewProject";
 import NoProjectSelected from "./components/NoProjectSelected";
 import ProjectsSidebar from "./components/ProjectSidebar";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+
+  function handleProjectStateOnCreate() {
+    setProjectState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null
+      }
+    })
+  }
+
+  let content;
+
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onProjectCreate={handleProjectStateOnCreate} />
+  }
+
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar />
-      <NoProjectSelected />
+      <ProjectsSidebar onProjectCreate={handleProjectStateOnCreate} />
+      {content}
     </main>
   );
 }
--- src/components/ProjectSidebar.jsx
+++ src/components/ProjectSidebar.jsx
@@ -1,6 +1,6 @@
 import Button from "./Button";
 
-export default function ProjectsSidebar() {
+export default function ProjectsSidebar({ onProjectCreate }) {
   return (
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl">
       <h2 className="mb-8 font-bold uppercase text-stone-200 text-xl md:text-xl">Your Projects</h2>
@@ -8,7 +8,7 @@ export default function ProjectsSidebar() {
       {/* Placeholder for project list (to be added later) */}
 
       <div>
-        <Button>+ Add Project</Button>
+        <Button onClick={onProjectCreate}>+ Add Project</Button>
       </div>
     </aside>
   );
